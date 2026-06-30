d58d511b35eecc903ce1ed9c83501538c6022234
Ahmad Raza <iahmad6raza@gmail.com>
2026-06-30 18:25:06 +0500

    fix: correct case-sensitive import for Order model

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Book = require('../models/Book')
+const Book = require('../models/book')
 const auth = require('../middleware/auth')
 
 // GET all books
